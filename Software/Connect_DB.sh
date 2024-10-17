255eb175a0fc6202fcbc3c6b3a10a5d68e452ef2
karim-mohamedd <karimmohamed5555555@gmail.com>
2024-10-17 05:13:32 +0300

    Connect

--- Software/Connect_DB.sh
+++ Software/Connect_DB.sh
@@ -1,61 +1,57 @@
 #!/usr/bin/bash
-cd ../DATA
+
+cd ../DATA || { echo "Failed to change directory to ../DATA"; exit 1; }
+
 echo
-PS3=">>>>>>  Type your DB number to connect with: "
+PS3=">>>>>> Type your DB number to connect with: "
 echo
 echo "================= Select The Data Base ================="
 echo
-array=(`ls -F | grep / | tr / ' '`)
-select choice in ${array[*]}
-do
-if [ $REPLY -gt ${#array[*]} ]
-then 
-    echo "============== it is not in the menue ============="
-    continue
-else 
-    cd ${array[${REPLY}-1]}
-    echo
-    echo " ----You are Connected to ${array[${REPLY}-1]} Suceesfully----"
-    echo
-    break
-fi
+
+# Get a list of directories (databases) and store them in an array
+array=($(ls -F | grep '/$' | tr -d '/'))
+
+# Use select to allow the user to choose a database
+select choice in "${array[@]}"; do
+    if [ -z "$choice" ]; then
+        echo "============== It is not in the menu ============="
+        continue
+    else
+        cd "$choice" || { echo "Failed to enter database $choice"; exit 1; }
+        echo
+        echo "---- You are connected to $choice successfully ----"
+        echo
+        break
+    fi
 done
 
-select choice in Create_Table List_Tables Drop_Table Insert_in_Table Select_From_Table Delete_From_Table Update_Table
-do 
+# Main menu for table operations
+select choice in Create_Table List_Tables Drop_Table Insert_in_Table Select_From_Table Delete_From_Table Update_Table; do 
     case $choice in 
-    Create_Table )
-        echo "Creating Table"
-        ../../Software/create_table.sh
-        ;;
-    List_Tables )
-        echo "Listing The Tables"
-        ../../Software/list_tables.sh
-        ;;
-    Drop_Table )
-        echo "Deleteing Table"
-        ../../Software/delete_table.sh
-        ;;
-    Insert_in_Table )
-        echo "Inserting in Table"
-        ../../Software/insert_table.sh
-        ;;
-    Select_From_Table )
-        echo "Selecting from Table"
-        ../../Software/select_table.sh
-        ;;
-    Delete_From_Table )
-        echo "Delete From Table"
-        ../../Software/delete.sh
-        ;;
-    Update_Table )
-        echo "Updating Table"
-        ../../Software/update_table.sh
-        ;;
-    * )
-        echo "Invalid input Please Try Again"
-    continue
-    ;;
+        Create_Table )
+            echo "Creating Table"
+            source ../../Software/create_table.sh
+            ;;
+        List_Tables )
+            echo "Listing The Tables"
+            ../../Software/list_tables.sh
+            ;;
+        Drop_Table )
+            echo "Deleting Table"
+            ../../Software/delete_table.sh
+            ;;
+
+        Delete_From_Table )
+            echo "Deleting From Table"
+            ../../Software/delete.sh
+            ;;
+
+        * )
+            echo "Invalid input. Please try again."
+            continue
+            ;;
     esac
 done
-cd - &> ~/../../dev/null
+
+# Return to the previous directory
+cd - &> /dev/null
