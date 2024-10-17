a60fdf6677a9c85a13dbe21281609ec1e8c94192
karim-mohamedd <karimmohamed5555555@gmail.com>
2024-10-17 05:15:17 +0300

    delete_table.sh

---
+++ Software/delete_table.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/bash
+
+PS3=">>>>>>> Type the Table number to Drop: "
+
+echo "---------- Select from the menu ---------------"
+
+# Store the list of tables in an array
+array=($(ls))
+
+# Use select to let the user choose a table
+select choice in "${array[@]}"; do
+    if [[ -z "$choice" ]]; then
+        echo "------- Try again: $REPLY is not in the menu -------"
+        continue
+    else 
+        # Remove the selected table
+        rm "$choice"
+        echo "$choice has been removed successfully."
+        break
+    fi 
+done
