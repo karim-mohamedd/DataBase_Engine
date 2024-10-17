9bba328beecc90d7c9c5bc4d079a12ed48fd4e2d
karim-mohamedd <karimmohamed5555555@gmail.com>
2024-10-17 05:14:33 +0300

    DataBase.sh

--- Software/DataBase.sh
+++ Software/DataBase.sh
@@ -1,26 +1,29 @@
 #!/usr/bin/bash
-PS3=">>>>>> "
-select choice in Create-DB list-DB Connect-DB Drop-DB
-do 
-case $choice in 
-Create-DB )
-    . create_DB.sh
-    ;;
 
-list-DB )
-    echo "The available Data Bases are : "
-    ./list_DB.sh
-    ;;
-Connect-DB )
-    echo "Connecting to the selected Data Base "
-    ./Connect_DB.sh
-    ;;
-
-Drop-DB )
-    echo "Removing The Selected Data Base"
-    ./Drop_DB.sh
-    ;;
+PS3=">>>>>> Please select an option: "
+select choice in Create-DB list-DB Connect-DB Drop-DB; do 
+    case $choice in 
+        Create-DB )
+            ./create_DB.sh
+            ;;
+        
+        list-DB )
+            echo "The available databases are:"
+            ./list_DB.sh
+            ;;
+        
+        Connect-DB )
+            echo "Connecting to the selected database..."
+            ./Connect_DB.sh
+            ;;
+        
+        Drop-DB )
+            echo "Removing the selected database..."
+            ./Drop_DB.sh
+            ;;
+        
+        * )
+            echo "Invalid option. Please try again."
+            ;;
     esac
 done
-
-
